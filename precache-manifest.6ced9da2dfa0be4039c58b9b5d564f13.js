1e42fd39d914857040e06b67ed7e67d7a9d8c8f1
jakob <drusany.jakob@gmail.com>
2020-10-22 14:28:53 +0200

    Updates

---
+++ precache-manifest.6ced9da2dfa0be4039c58b9b5d564f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cb4de33ac4900744bb1049f5c0d075",
+    "url": "/fri-fmf-timetable/index.html"
+  },
+  {
+    "revision": "35bde80adc8f1fb87e51",
+    "url": "/fri-fmf-timetable/static/css/2.8c462a9d.chunk.css"
+  },
+  {
+    "revision": "240e97133c15e5e9fab0",
+    "url": "/fri-fmf-timetable/static/css/main.d423b8d3.chunk.css"
+  },
+  {
+    "revision": "35bde80adc8f1fb87e51",
+    "url": "/fri-fmf-timetable/static/js/2.e939d7fb.chunk.js"
+  },
+  {
+    "revision": "7c514825c1a9c4a61543a783549fbf2b",
+    "url": "/fri-fmf-timetable/static/js/2.e939d7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240e97133c15e5e9fab0",
+    "url": "/fri-fmf-timetable/static/js/main.6e7c5c39.chunk.js"
+  },
+  {
+    "revision": "e04e9c3d9d72baac8a07",
+    "url": "/fri-fmf-timetable/static/js/runtime-main.a81909d4.js"
+  }
+]);
